5bc4004b3d306edcc22bb30d67e35d6e351aa065
wocjs <wocjs602@gmail.com>
2023-08-31 17:49:42 +0900

    add 14567_h

---
+++ Code/14567/14567_H.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+#####
+n, m = map(int, input().split())
+arr = [[] for _ in range(n)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    arr[a].append(b)
+
+ans = [1] * (n + 1)
+for i in range(n):
+    for connection in arr[i]:
+        ans[connection] = max(ans[connection], ans[i]+1)
+print(*ans[1:])
