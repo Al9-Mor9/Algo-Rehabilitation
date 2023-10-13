a917d8c7a7cbd8bbe7cf9a5f0594a1b048c8febd
wocjs <wocjs602@gmail.com>
2023-10-13 12:45:17 +0900

    [Update] 17484_H

--- Code/17484/17484_H.py
+++ Code/17484/17484_H.py
@@ -3,18 +3,20 @@ sys.stdin = open('input.txt', 'r')
 #####
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-dr = [-1, 0, 1]
 ans = 600
-## dfs 로 풀어서 끝까지 갔을 때 min값 update
-# directions?
-def dfs(row, col, dir, fuel):
+dr = [-1, 0, 1]
+
+def dfs(row, col, d, fuel):
     global ans
+
     if row == n:
-        ans = min(ans, fuel)
+        ans = min(fuel, ans)
         return
-    for i in dr:
-        if i != dir and 0 <= col + i < m:
-            dfs(row+1, col+i, i, fuel + arr[row][col])
+    for dir in dr:
+        if d != dir and 0 <= col + dir < m:
+            dfs(row+1, col+dir, dir, fuel + arr[row][col])
+
+
 
 for j in range(m):
     dfs(0, j, 2, 0)
