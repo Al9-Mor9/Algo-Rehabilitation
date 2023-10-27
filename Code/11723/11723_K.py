ce1118ea1affb4e432017c863f16db7ed34b15e7
Haaarimmm <haaarimmm@gmail.com>
2023-10-27 22:52:23 +0900

    daily algo

---
+++ Code/11723/11723_K.py
@@ -0,0 +1,32 @@
+# 집합
+import sys
+from collections import deque
+sys.stdin = open('input.txt', 'r')
+
+input = sys.stdin.readline
+m = int(input())
+S = deque()
+
+for _ in range(m):
+    cmd = input().rstrip('\n')
+    if cmd == 'all':
+        S = [i for i in range(1, 21)]
+    elif cmd == 'empty':
+        S = deque()
+    else:
+        subcmd, num = cmd.split()
+        num = int(num)
+        if subcmd == 'add' and num not in S:
+            S.append(num)
+        elif subcmd == 'remove' and num in S:
+            S.remove(num)
+        elif subcmd == 'check':
+            if num in S:
+                print(1)
+            else:
+                print(0)
+        elif subcmd == 'toggle':
+            if num in S:
+                S.remove(num)
+            else:
+                S.append(num)
