ea0a197ea8f37fdaa473f95fd46360c0fc8dabf3
Haaarimmm <haaarimmm@gmail.com>
2023-11-04 15:29:43 +0900

    update

---
+++ Code/7568/7568_K.py
@@ -0,0 +1,18 @@
+# 덩치
+import sys
+sys.stdin = open('input.txt', 'r')
+
+N = int(input())
+lst = [list(map(int, input().split())) + [i] for i in range(N)]
+lst.sort(key=lambda x:(x[0], x[1]), reverse=True)
+
+ans = [0] * N
+for i in range(N):
+    rank = 1
+    w1, h1 = lst[i][0], lst[i][1]
+    for j in range(i):
+        w2, h2 = lst[j][0], lst[j][1]
+        if w2 > w1 and h2 > h1:
+            rank += 1
+    ans[lst[i][2]] = rank
+print(*ans)
